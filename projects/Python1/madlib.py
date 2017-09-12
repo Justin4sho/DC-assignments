612547829bc4ac15b76fc3f180f88c8613050d60
Justin Boyd <jrboyd90@gmail.com>
2017-09-12 15:11:31 -0500

    completed

--- projects/Python1/madlib.py
+++ projects/Python1/madlib.py
@@ -1,1 +1,6 @@
-mad
+print('Please fill in the blanks below:\n____(name)____\'s favorite subject in school is ___(subjext)___.')
+
+name = input('What is name? ')
+subject = input('What is subject? ')
+
+print(name, '\'s favorite subject in school is', subject, '.') 
