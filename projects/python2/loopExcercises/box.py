def9261b8d2c15bad3037ef06649dd433f915b7e
Justin Boyd <jrboyd90@gmail.com>
2017-09-14 10:57:32 -0500

    wip

--- projects/python2/loopExcercises/box.py
+++ projects/python2/loopExcercises/box.py
@@ -1,11 +1,11 @@
 width = int(input('Width? '))
 height = int(input('Height? '))
 
-for i in range(0,width):
-    if i == 0 or i == width:
-        print('*' * width)
-    else:
-        for j in range(0,height):
-            if j == 0 or i == height:
-                print('*')
-            
+print('*' * width)
+
+spaces = width - 2
+spaces = ' ' * spaces
+for i in range(height - 2):
+    print('*' + spaces + '*')
+
+print('*' * width)
---
+++ projects/python2/loopExcercises/triangle.py
@@ -0,0 +1,10 @@
+spaces = ' '
+star = '*'
+height = 4
+width = 7
+
+
+for i in range(height - 1):
+    count = 1
+    print(spaces * (height - 1), star * (i * count) , spaces * (height - 1)
+    
