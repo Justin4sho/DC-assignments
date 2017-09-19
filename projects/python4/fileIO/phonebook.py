32f87bc6089de6bc0829abd91db9968d3de8d66f
Justin Boyd <jrboyd90@gmail.com>
2017-09-19 09:48:15 -0500

    Completed

--- projects/python4/fileIO/phonebook.py
+++ projects/python4/fileIO/phonebook.py
@@ -1,10 +1,13 @@
 from phoneFunctions import *
+import pickle
+
+
 phonebook = {}
 
 num = ''
 string = ''
 while True:
-    selection = int(input('\nElectronic Phone Book\n=====================\n1. Look up an entry\n2. Set an entry\n3. Delete an entry\n4. List all entries\n5. Quit\nWhat do you want to do (1-5)? '))
+    selection = int(input('\nElectronic Phone Book\n=====================\n1. Look up an entry\n2. Set an entry\n3. Delete an entry\n4. List all entries\n5. Save entries\n6. Restore saved entries\n7. Quit\nWhat do you want to do (1-7)? '))
     if selection == 1:
         string = input('Name: ')
 
@@ -21,5 +24,12 @@ while True:
         for key, value in phonebook.items():
             print('Found entry for ', key, ': ', value)
     elif selection == 5:
+        myfile = open('phonebook.pickle', 'wb')
+        pickle.dump(phonebook, myfile)
+        myfile.close()
+    elif selection == 6:
+        myfile = open('phonebook.pickle', 'rb')
+        phonebook = pickle.load(myfile)
+    elif selection == 7:
         print('Bye')
         break
