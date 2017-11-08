5647336a599b55729b75a66b1b104e21bdcd7b8b
Justin Boyd <jrboyd90@gmail.com>
2017-11-08 11:34:36 -0600

    added index

--- projects/restaurant/app.js
+++ projects/restaurant/app.js
@@ -10,9 +10,29 @@ var db = pgp({database: 'restaurant'});
 app.use(body_parser.urlencoded({extended: false}));
 // sets up handlbars
 app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
 
 //renders home page
 app.get('/', function (req, resp) {
-  resp.send('restaurant');
+  var context = {
+    title: 'Restaurant Reviews'
+  }
+  resp.render('index.hbs',context);
+});
+//renders search results
+app.get('/search', function (req, resp) {
+  var searchTerm = req.query.searchTerm;
+  var context = {
+    title: 'Restaurants'
+  }
+  resp.render('search_results.hbs',context);
+
+});
+
+
+
+//starts up server on port 8000
+app.listen(8888, function () {
+  console.log('Listening on port 8888');
 });
