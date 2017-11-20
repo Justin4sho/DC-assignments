f5896e549a3e0162a7d7c0c15ec5ed6ea0140a07
Justin Boyd <jrboyd90@gmail.com>
2017-11-20 11:55:03 -0600

    MUI

--- projects/contacts/src/App.js
+++ projects/contacts/src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <h1>
+        Hi
+      </h1>
+    )
   }
 }
 
